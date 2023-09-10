c50d68f5b032f0dbbe9f9855080a16246a0b997c
robert <450130348@qq.com>
2023-09-10 21:27:25 +0800

    modify main.cpp, show the sphere normals as color

--- main.cpp
+++ main.cpp
@@ -9,12 +9,22 @@ bool hit_sphere(const point3& sphere_center, double radius, const ray& r) {
     auto b = 2.0 * dot(co, r.direction());
     auto c = dot(co, co) - radius * radius;
     auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+
+    if (discriminat < 0) {
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant)) / (2.0*a);
+    }
 }
 
 color ray_color(const ray& r) {
-    if(hit_sphere(point3(0,0,-1), 0.5, r)) {
-        return color(1,0,0);
+    point3 sphere_center = point3(0,0,-1);
+    double radius = 0.5;
+
+    auto t = hit_sphere(sphere_center, radius, r);
+    if (t > 0.0) {
+        vec3 normal = unit_vector(r.at(t) - sphere_center);
+        return 0.5 * color(normal.x()+1, normal.y()+1, normal.z()+1);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
