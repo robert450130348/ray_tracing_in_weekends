196ce9d4952c6c9e7f936ead3cfb6e76979aaf25
robert <450130348@qq.com>
2023-09-10 21:20:16 +0800

    modify main.cpp, add a sphere to the scene

--- main.cpp
+++ main.cpp
@@ -3,7 +3,20 @@
 #include "vec3.h"
 #include "ray.h"
 
+bool hit_sphere(const point3& sphere_center, double radius, const ray& r) {
+    vec3 co = r.origin() - sphere_center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(co, r.direction());
+    auto c = dot(co, co) - radius * radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+    if(hit_sphere(point3(0,0,-1), 0.5, r)) {
+        return color(1,0,0);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0,1.0,1.0) + a*color(0.5,0.7,1.0);
